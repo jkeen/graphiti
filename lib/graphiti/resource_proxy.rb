e06783b6cc4422a17b7a4bf226c8dd77a75f7394
Jeff Keen <jeff@keen.me>
2021-05-26 10:18:07 -0500

    Provide access to underlying model during build/update/save process, closer mirroring rails controller patterns.
    Changes:
     - (for creates):
    	@resource = MyResource.build(params);
    	@resource.data #=> unsaved model with attributes applied
     - (for updates):
    	@resource = MyResource.find(params);
    	@resource.assign_attributes
    	@resource.data #=> unsaved model with attributes applied

--- lib/graphiti/resource_proxy.rb
+++ lib/graphiti/resource_proxy.rb
@@ -73,7 +73,9 @@ module Graphiti
         records
       end
     end
-    alias_method :to_a, :data
+
+    alias to_a data
+    alias resolve_data data
 
     def meta
       @meta ||= data.respond_to?(:meta) ? data.meta : {}
@@ -103,6 +105,16 @@ module Graphiti
 
     def pagination
       @pagination ||= Delegates::Pagination.new(self)
+    end
+
+    def assign_attributes(params = nil)
+      # deserialize params again?
+
+      @data = @resource.assign_with_relationships(
+        @payload.meta,
+        @payload.attributes,
+        @payload.relationships,
+      )
     end
 
     def save(action: :create)
@@ -136,7 +148,7 @@ module Graphiti
     end
 
     def destroy
-      data
+      resolve_data
       transaction_response = @resource.transaction do
         metadata = {method: :destroy}
         model = @resource.destroy(@query.filters[:id], metadata)
@@ -154,7 +166,8 @@ module Graphiti
     end
 
     def update
-      data
+      resolve_data
+      assign_attributes
       save(action: :update)
     end
 
